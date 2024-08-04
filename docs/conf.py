fae719efdb498378650f23b22d6969f34cbcfc39
Tom Pike <tpike3@gmu.edu>
2024-08-04 16:33:30 -0400

    update to requirements
    remove jupyterlite extnesion

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,6 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
-    "jupyterlite_sphinx"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
