2320158aada98e9035921f9a09673766081861d5
Tom Pike <tpike3@gmu.edu>
2024-08-05 06:54:39 -0400

    back to conda

--- docs/conf.py
+++ docs/conf.py
@@ -80,6 +80,8 @@ create_new_kernel_directory_and_update_json()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_nb",
+    "jupyterlite_sphinx",
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
